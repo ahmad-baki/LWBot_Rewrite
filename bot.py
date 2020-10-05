18538a47128ba289f05973b6198505f223eca6a6
Florik3ks <floritrs@gmail.com>
2020-10-05 10:34:19 +0200

    subprocess for ev cmd

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import asyncio
 import datetime
 import operator
 import os
+import subprocess
 
 import lwConfig
 import lwHelperFunctions
@@ -86,7 +87,8 @@ async def _eval(ctx, *, arg):
                 "discord": discord,
                 "datetime": datetime,
                 "reminderHandler": reminderHandler,
-                "os": os
+                "os": os,
+                "subprocess": subprocess
             })
         except Exception as e:
             if isinstance(e, TypeError):

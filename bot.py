75b0a5ef42d33cd36cdbdc56b24a17b7ff71ae84
Florik3ks <floritrs@gmail.com>
2021-01-20 16:02:13 +0100

    added custom UserMessages Feature

--- bot.py
+++ bot.py
@@ -627,7 +627,7 @@ class Memes(commands.Cog):
                     bot, config.UNDERSTOOD_EMOJI)
                 await errormsg.add_reaction(deleteEmoji)
                 try:
-                    reaction, user = await bot.wait_for('reaction_add', timeout=30.0, check=lambda _reaction, _user: _user == user and _reaction.emoji.name == deleteEmoji.name)
+                    reaction, user = await bot.wait_for('reaction_add', timeout=30.0, check=lambda _reaction, _user: _user == user and _reaction.emoji == deleteEmoji)
                 except futures.TimeoutError:
                     pass
                 await errormsg.delete()
@@ -840,6 +840,7 @@ class Wholesome(commands.Cog):
     """Wholesome commands um deine Seele zu reinigen"""
     def __init__(self, bot):
         self.bot = bot
+        self.cmds = ["hug", "pat", "feed", "cuddle", "kiss", "poke", "slap", "bite"]
 
     @commands.command()
     async def hug(self, ctx, *args):
@@ -881,8 +882,6 @@ class Wholesome(commands.Cog):
     async def bite(self, ctx, *args):
         """beiße einen anderen Benutzer mit `bite @user`"""
         await self.send(ctx, args, "bite", "gebissen", cat_ascii="(・∀・)")
-
-
 
 
     async def send(self, ctx, args, command, verb, content_type="gif", cat_ascii="(^･o･^)ﾉ”"):
@@ -930,7 +929,7 @@ class Wholesome(commands.Cog):
             await ctx.send(embed=simpleEmbed(ctx.author, "Es müssen genau zwei Argumente übergeben werden", "Beispiel: `add hug hug_gif.gif`", color=discord.Color.red()))
             return
         category = args[0]
-        if category not in ["hug","pat","poke", "slap"]:
+        if category not in self.cmds:
             await ctx.send(embed=simpleEmbed(ctx.author, "Die angegebene Kategorie ist nicht vorhanden", color=discord.Color.red()))
             return
         gif = args[1]
@@ -1072,9 +1071,107 @@ class Ahmad(commands.Cog):
                 await after.edit(name="Ahmad-Kult")
 
 
+class UserMessages(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.data = self.read_json()
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        js = self.data
+        if str(message.author.id) in js.keys():
+            if message.content in js[str(message.author.id)].keys():
+                await message.channel.send(random.choice(js[str(message.author.id)][message.content]))
+
+    
+    @commands.command(aliases=["mm"])
+    async def myMessages(self, ctx, *args):
+        """Lasse deine eingestellten Nachrichten aufzählen"""
+        e = discord.Embed(title="Deine Nachrichten", color=ctx.author.color)
+        if str(ctx.author.id) in self.data.keys():
+            for k in self.data[str(ctx.author.id)].keys():
+                e.add_field(name=str(k), value=str(self.data[str(ctx.author.id)][k]), inline=False)
+        else:
+            e.description = "Du hast keine eigenen Nachrichten eingestellt."
+        await ctx.send(embed=e)
+
+    @commands.command(aliases=["am"])
+    async def addMessage(self, ctx, *args):
+        """Füge eine neue Reaktion auf eine bestimmte Nachricht von dir hinzu.
+        Die Syntax ist: `addMessage "deine Nachricht, falls es mehr als ein Wort ist in Hochkommas" "Die Reaktionsnachricht des Bots"`"""
+        if len(args) != 2:
+            await ctx.send(embed=simpleEmbed(ctx.author, "Du musst genau zwei Argumente angeben",
+                        'Die Syntax ist: `addMessage "deine Nachricht, falls es mehr als ein Wort ist in Hochkommas" "Die Reaktionsnachricht des Bots"`',
+                        color=discord.Color.red()))
+        self.add_in_json(ctx.author, args[0], args[1])
+        e = simpleEmbed(ctx.author, "Erfolgreich hinzugefügt!", color=ctx.author.color)
+        e.description = f"`{args[1]}` wurde erfolgreich `{args[0]}` hinzugefügt."
+        await ctx.send(embed=e)
+
+
+    @commands.command(aliases=["rm"])
+    async def removeMessage(self, ctx, *args):
+        """Lösche eine bereits existierende Reaktion auf eine bestimmte Nachricht von dir."""
+        if str(ctx.author.id) in self.data.keys():
+            if len(args) != 1:
+                await ctx.send(embed=simpleEmbed(ctx.author, "Du musst genau ein Argument angeben",
+                            'Die Syntax ist: `removeMessage "Nachricht, die gelöscht werden soll (in Hochkommas, falls mehr als ein Wort)"`',
+                            color=discord.Color.red()))
+            else:
+                if args[0] in self.data[str(ctx.author.id)].keys():
+                    self.remove_from_json(ctx.author, args[0])
+                    e = simpleEmbed(ctx.author, "Erfolgreich entfernt!", color=ctx.author.color)
+                    e.description = f"`{args[0]}` wurde erfolgreich entfernt."
+                    await ctx.send(embed=e)
+                else:
+                    await ctx.send(embed=simpleEmbed(ctx.author, "Diese Nachricht befindet sich nicht in deinen Nachrichten.", color=discord.Color.red()))
+
+        else:
+            await ctx.send(embed=simpleEmbed(ctx.author, "Du hast keine eigenen Nachrichten eingestellt.", color=discord.Color.red()))
+
+    
+    def read_json(self):
+        try:
+            with open(config.path + f'/json/userReactions.json', 'r') as myfile:
+                return json.loads(myfile.read())
+        except FileNotFoundError:
+            return {}
+
+    def add_in_json(self, user, on_msg, bot_reaction_msg):
+        js = self.data
+        try:
+            with open(config.path + '/json/userReactions.json', 'w') as myfile:
+                if not str(user.id) in js.keys():
+                    js[str(user.id)] = {}
+                if not on_msg in js[str(user.id)].keys():
+                    js[str(user.id)][on_msg] = []
+                js[str(user.id)][on_msg].append(bot_reaction_msg)
+                json.dump(js, myfile)
+        except FileNotFoundError:
+            file = open(config.path + '/json/userReactions.json', 'w')
+            file.write("{}")
+            file.close()
+            self.add_in_json(user, on_msg, bot_reaction_msg)
+        self.data = js
+
+    def remove_from_json(self, user, key):
+        js = self.data
+        try:
+            with open(config.path + '/json/userReactions.json', 'w') as myfile:
+                del js[str(user.id)][key]
+                json.dump(js, myfile)
+        except FileNotFoundError:
+            file = open(config.path + '/json/userReactions.json', 'w')
+            file.write("{}")
+            file.close()
+        self.data = js
+
+
+
 bot.add_cog(Erinnerungen(bot))
 bot.add_cog(Stundenplan(bot))
 bot.add_cog(Wholesome(bot))
+bot.add_cog(UserMessages(bot))
 bot.add_cog(Memes(bot))
 bot.add_cog(Utility(bot))
 bot.add_cog(Debug(bot))

e72bf2e6733b2a867127952b5fd30ed08d2b972d
Florik3ks <floritrs@gmail.com>
2021-02-08 14:35:45 +0100

    error handler for scraper cog

--- cogs/scraper.py
+++ cogs/scraper.py
@@ -1,5 +1,4 @@
 import json
-from os import scandir
 import requests
 from bs4 import BeautifulSoup
 import discord
@@ -7,6 +6,7 @@ from discord.ext import commands
 from discord.ext import tasks
 
 import config
+from helper_functions import *
 from bot import on_command_error
 
 
@@ -20,34 +20,6 @@ class Anzeige():
         self.description = description
         self.url = url
 
-
-def get_ads(config):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59'
-    }
-    r = requests.get(url=f'{config["url"]}r{config["radius"]}', headers=headers)
-    soup = BeautifulSoup(r.text, features="html5lib")
-    results = soup.find("div", id="srchrslt-content")
-    ads = []
-    for i in results.find_all("article"):
-        ad = Anzeige()
-        ad.id = i["data-adid"]
-        details = i.find("div", {"class": "aditem-details"})
-        ad.price = details.find("strong").text
-        radius = details.contents[8].split()[-2:]
-        if int(radius[0]) > config["radius"]:
-            continue
-        ad.location = f"{details.contents[4].split()[-1]} {details.contents[6].split()[-1]} ({' '.join(radius)})"
-        ad.description = i.find(
-            "div", {"class": "aditem-main"}).contents[3].contents[0]
-        ad.time = ' '.join(
-            i.find("div", {"class": "aditem-addon"}).contents[0].split())
-
-        title = i.find("a", {"class": "ellipsis"})
-        ad.title = title.contents[0]
-        ad.url = config["base_url"] + title["href"]
-        ads.append(ad)
-    return ads
 
 
 def ad_to_embed(ad: Anzeige):
@@ -70,11 +42,41 @@ class Scraper(commands.Cog):
             "base_url": "https://www.ebay-kleinanzeigen.de",
             "radius": 150
         }
-        self.scraper.start()
+        if config.PREFIX == ",":
+            self.scraper.start()
 
+        
+    def get_ads(self, config):
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59'
+        }
+        r = requests.get(url=f'{config["url"]}r{config["radius"]}', headers=headers)
+        soup = BeautifulSoup(r.text, features="html5lib")
+        results = soup.find("div", id="srchrslt-content")
+        ads = []
+        for i in results.find_all("article"):
+            ad = Anzeige()
+            ad.id = i["data-adid"]
+            details = i.find("div", {"class": "aditem-details"})
+            ad.price = details.find("strong").text
+            radius = details.contents[8].split()[-2:]
+            if int(radius[0]) > config["radius"]:
+                continue
+            ad.location = f"{details.contents[4].split()[-1]} {details.contents[6].split()[-1]} ({' '.join(radius)})"
+            ad.description = i.find(
+                "div", {"class": "aditem-main"}).contents[3].contents[0]
+            ad.time = ' '.join(
+                i.find("div", {"class": "aditem-addon"}).contents[0].split())
+
+            title = i.find("a", {"class": "ellipsis"})
+            ad.title = title.contents[0]
+            ad.url = config["base_url"] + title["href"]
+            ads.append(ad)
+        return ads
+    
     @tasks.loop(seconds=300)
     async def scraper(self):
-        ads = get_ads(self.config)
+        ads = self.get_ads(self.config)
         with open(config.path + '/json/user_config.json', 'r') as myfile:
             data = json.loads(myfile.read())
 
@@ -95,6 +97,10 @@ class Scraper(commands.Cog):
     async def beforeReminderCheck(self):
         await self.bot.wait_until_ready()
 
-
+    @scraper.error
+    async def scraper_error(self, error):
+        channel = self.bot.get_channel(config.LOG_CHANNEL_ID)
+        await channel.send(embed=simple_embed(self.bot.user, "scraper error", color=discord.Color.orange()))
+        await on_command_error(self.bot.get_channel(config.LOG_CHANNEL_ID), error)
 def setup(bot):
     bot.add_cog(Scraper(bot))
